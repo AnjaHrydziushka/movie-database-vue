94e1a8037fc47d3fb8ea0701ee471f1b2d9953be
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-11 20:32:18 +0200

    Deleted HelloWorld, added component, Tailwind CSS

---
+++ src/components/OneMovie.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="movie">
+    <h1>One movie</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OneMovie",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  padding: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Movie Database with Vue" />
     <AllMovies />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import AllMovies from "@/components/AllMovies.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     AllMovies
   }
 };
