2be543b05eaaa00ca9fd7bff99e7e82614226025
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-11 18:45:21 +0200

    Changed image on Home page and component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Popcorn" src="../assets/movie.png" />
+    <HelloWorld msg="Movie Database with Vue" />
   </div>
 </template>
 
