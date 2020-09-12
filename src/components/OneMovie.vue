83211e70e23b5bc1faf2b6950553b0f96dac5ae9
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-12 21:22:14 +0200

    Displayed data for one movie page

--- src/components/OneMovie.vue
+++ src/components/OneMovie.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="movie">
-    <h1>Page of the movie with ID</h1>
+  <div class="movie" v-if="movie">
+    <h1>{{ movie.title }}</h1>
+    <p>
+      <em>{{ movie.year }} {{ movie.genre }}</em>
+    </p>
+    <img :src="movie.image" :alt="movie.title" width="300px" />
+    <p>{{ movie.director }}</p>
+    <p>{{ movie.description }}</p>
   </div>
 </template>
 
