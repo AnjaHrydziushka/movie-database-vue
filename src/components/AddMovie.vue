bd9e3bb8821eaebc334a4d3a105906cbff9279fb
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-13 12:16:33 +0200

    Created a simple form

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <h1>Add a new movie</h1>
+    <form>
+      <p>
+        <label for="title">Title </label>
+        <input id="title" />
+      </p>
+      <p>
+        <label for="year">Year </label>
+        <input id="year" />
+      </p>
+      <p>
+        <label for="genre">Genre </label>
+        <input id="genre" />
+      </p>
+      <p>
+        <label for="director">Director </label>
+        <input id="director" />
+      </p>
+      <p>
+        <label for="description">Description </label>
+        <textarea id="description" />
+      </p>
+      <p>
+        <input type="submit" value="Submit" />
+      </p>
+    </form>
   </div>
 </template>
 
