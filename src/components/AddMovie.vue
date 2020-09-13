047ba239b9b1c96916914845131a71b08ca68e14
AnjaHrydziushka <anja.hryd@gmail.com>
2020-09-13 12:09:12 +0200

    Created a new page, component and route for adding a movie

---
+++ src/components/AddMovie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Add a new movie</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      movies: []
+    };
+  },
+  mounted() {
+    this.movies = this.$store.getters.getMovies;
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  padding: 0;
+}
+</style>
